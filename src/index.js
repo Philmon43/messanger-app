62f225bb55c51cf09bce2715e3dd2ec2446becff
philmon43 <philmon43@gmail.com>
2021-05-12 12:02:13 +0300

    heroku

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-require("./db/mongoose");
+require("./db/mongoose")
 require('dotenv').config()
-const express = require("express");
+const express = require("express")
 const userRouter = require("./routers/user")
-const taskRouter = require("./routers/tasks");
+const taskRouter = require("./routers/tasks")
 const cors = require('cors')
 const path = require("path")
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 app.use(userRouter)
 app.use(taskRouter)
-const port = process.env.PORT || 5000;
-
-const publicDirectory = path.join(__dirname, "../client/build");
-
-// console.log(path.join(__dirname, "../client/build"))
+const port = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(publicDirectory))
+    app.use(express.static("client/build"))
 }
 
 app.listen(port, () => console.log("Server is up on port ", port));
