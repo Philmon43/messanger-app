d1331024755cd76ac6260adf32b58179479ab295
philmon43 <philmon43@gmail.com>
2021-05-12 12:40:33 +0300

    heroku error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express")
 const userRouter = require("./routers/user")
 const taskRouter = require("./routers/tasks")
 const cors = require('cors')
+const port = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
@@ -11,7 +12,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(userRouter)
 app.use(taskRouter)
-const port = process.env.PORT || 5000
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
