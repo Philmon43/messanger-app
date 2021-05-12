29e0091cdfbe7e935bae113920599ae15156da06
philmon43 <philmon43@gmail.com>
2021-05-12 10:53:58 +0300

    heroku

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const userRouter = require("./routers/user")
 const taskRouter = require("./routers/tasks");
 const cors = require('cors')
+const path = require("path")
 
 const app = express();
 app.use(express.json());
@@ -12,8 +13,10 @@ app.use(userRouter)
 app.use(taskRouter)
 const port = process.env.PORT || 5000;
 
+const publicDirectory = path.join(__dirname, "client/build");
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"))
+    app.use(express.static(publicDirectory))
 }
 
 app.listen(port, () => console.log("Server is up on port ", port));
